a099ab1f6417f5b02a4dcfdbee3070334001ad57
Pcfm17 <paulocesarfmotta17@gmail.com>
2024-11-02 14:02:16 -0300

    novos

--- Python/Andre prof/secao06/12.py
+++ Python/Andre prof/secao06/12.py
@@ -1,1 +1,5 @@
 #Diferenças de for loop e while loop
+'''
+for loop: utilizar o for loop quando vc nao sabe quantas vezes vc pode fazer
+while loop: utilizar o while loop quando vc sabe quantas vezes vc pode fazer
+'''
--- Python/Andre prof/secao06/13.py
+++ Python/Andre prof/secao06/13.py
@@ -1,1 +1,12 @@
 #Criando condições com while loop
+'''
+1) excelente para loops dependetes de uma codicao
+2) publicar um produto com comissao de 10% se for acima de R$20
+'''
+
+valor = float(input('Custo do produto: '))
+
+while valor > 20:
+    comissao = valor * 0.1 + valor
+    print(f'O valor final do seu produto será de R$ {comissao:.2f}')
+    break
